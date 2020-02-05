373013817fd01422c77efb08374b0e72d6955009
Kogia-sima <orcinus4627@gmail.com>
2020-02-05 13:02:40 +0900

    Update prelude

--- gkquad/src/prelude.rs
+++ gkquad/src/prelude.rs
@@ -1,2 +1,2 @@
 pub use crate::{IntegrationResult, RuntimeError, Tolerance};
-pub use crate::single::{Integrator, Integrand, integrate, integrate_with_config};
+pub use crate::single::{Integrator, Integrand, integrate};
