7fd64d7a30cde80d74535990985f4138f8c42a54
Kogia-sima <orcinus4627@gmail.com>
2020-02-09 13:01:48 +0900

    fix: Fix prelude

--- gkquad/src/prelude.rs
+++ gkquad/src/prelude.rs
@@ -1,2 +1,5 @@
 pub use crate::{IntegrationResult, RuntimeError, Tolerance};
+
+#[cfg(feature = "single")]
+#[cfg_attr(docsrs, doc(cfg(feature = "single")))]
 pub use crate::single::{Integrator, Integrand, integral};
