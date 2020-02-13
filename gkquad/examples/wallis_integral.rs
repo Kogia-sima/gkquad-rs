83f0895e8564e5ede81c9d83af32c1ad82248620
Kogia-sima <orcinus4627@gmail.com>
2020-02-13 12:56:07 +0900

    feat: Add example

---
+++ gkquad/examples/wallis_integral.rs
@@ -0,0 +1,31 @@
+extern crate gkquad;
+
+use gkquad::single::algorithm::QAGS;
+use gkquad::single::Integrator;
+use gkquad::Tolerance;
+use std::cell::Cell;
+use std::f64::consts::PI;
+use std::time::Instant;
+
+fn main() {
+    let timer = Instant::now();
+    let m = Cell::new(1);
+    let mut integrator = Integrator::new(|x: f64| (x * PI).sin().powi(m.get()), QAGS::new())
+        .tolerance(Tolerance::Absolute(1.));
+    let expected = 36722.362174233774391;
+
+    for _ in 0..100000 {
+        m.set(1);
+        let mut product = 1.0;
+
+        for _ in 0..10 {
+            let estimate = integrator.run(0.0..1.0).estimate().unwrap();
+            product *= estimate;
+            m.set(m.get() + 1);
+        }
+
+        assert!((1. / product - expected).abs() < 1.);
+    }
+
+    println!("Elapsed time: {:?}", timer.elapsed() / 100000);
+}
