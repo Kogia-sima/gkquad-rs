d867d8fa3ff09af11811bcf13933b7a2bbb544bc
Kogia-sima <orcinus4627@gmail.com>
2020-02-28 13:38:21 +0900

    Add AUTO2 algorithm

---
+++ gkquad/src/double/algorithm/auto.rs
@@ -0,0 +1,34 @@
+use super::super::common::{Integrand2, IntegrationConfig2};
+use super::super::range::{DynamicY, Rectangle};
+use super::{Algorithm2, QAGP2, QAGS2};
+use crate::IntegrationResult;
+
+pub struct AUTO2;
+
+impl AUTO2 {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+macro_rules! impl_algorithm2 {
+    ($range:ty) => {
+        impl<'a, F: Integrand2> Algorithm2<F, $range> for AUTO2 {
+            fn integrate(
+                &mut self,
+                f: &mut F,
+                range: &$range,
+                config: &IntegrationConfig2,
+            ) -> IntegrationResult {
+                if config.points.is_empty() {
+                    QAGS2::new().integrate(f, range, config)
+                } else {
+                    QAGP2::new().integrate(f, range, config)
+                }
+            }
+        }
+    };
+}
+
+impl_algorithm2!(Rectangle);
+impl_algorithm2!(DynamicY<'a>);
