85d54df0e019d01a562d45b22ee4f63384412e9b
Kogia-sima <orcinus4627@gmail.com>
2020-03-05 00:13:47 +0900

    Add benchmarks for double integral

---
+++ gkquad/benches/double.rs
@@ -0,0 +1,47 @@
+#[macro_use]
+extern crate bencher;
+
+extern crate gkquad;
+
+use bencher::{black_box, Bencher};
+use std::f64::consts::PI;
+
+use gkquad::double::Integrator2;
+use gkquad::Tolerance;
+
+fn simple(b: &mut Bencher) {
+    let mut integrator = Integrator2::new(|x: f64, y: f64| x * y);
+    b.iter(|| {
+        let range = black_box((0.0..1.0, 0.0..1.0));
+        let result = integrator.run(range).unwrap().estimate;
+        assert!((result - 1.0 / 4.0).abs() <= 1.49e-8);
+    });
+}
+
+fn singular_points(b: &mut Bencher) {
+    let mut integrator = Integrator2::new(|x: f64, y: f64| (x * y).recip())
+        .points(&[(0., 0.)])
+        .tolerance(Tolerance::Absolute(1.49e-8));
+
+    b.iter(|| {
+        let range = black_box((-1.0..1.0, -1.0..1.0));
+        let result = integrator.run(range).unwrap().estimate;
+        assert!(result.abs() <= 1.49e-8);
+    });
+}
+
+fn infinite_range(b: &mut Bencher) {
+    let mut integrator = Integrator2::new(|x: f64, y: f64| {
+        let denom = 1.0 + x * x + y * y;
+        (denom * denom.sqrt()).recip()
+    });
+
+    b.iter(|| {
+        let range = black_box((.., ..));
+        let result = integrator.run(range).unwrap().estimate;
+        assert!((result - 2.0 * PI).abs() <= 1.49e-8);
+    });
+}
+
+benchmark_group!(benches, simple, singular_points, infinite_range);
+benchmark_main!(benches);
