55aba854de4cf5ce066d8695561f2eff402a95f1
Kogia-sima <orcinus4627@gmail.com>
2020-03-04 20:02:02 +0900

    Add documentations for ValueWithError and Solution

--- gkquad/src/common.rs
+++ gkquad/src/common.rs
@@ -53,6 +53,9 @@ impl Default for Tolerance {
 ///
 /// It is quite similar to `std::result::Result` type, but you can always
 /// extract the partial result by calling `unwrap_unchecked()` method.
+///
+/// This type is useful when the calculation must satisfy a some post-condition
+/// which is checked at runtime.
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord, Hash)]
 pub struct ValueWithError<T, E> {
     pub(crate) value: T,
@@ -60,11 +63,13 @@ pub struct ValueWithError<T, E> {
 }
 
 impl<T, E> ValueWithError<T, E> {
+    /// Create a new instance with no error
     #[inline]
     pub fn new(value: T) -> Self {
         Self { value, error: None }
     }
 
+    /// Create a new instance with given error
     #[inline]
     pub fn with_error(value: T, error: E) -> Self {
         Self {
@@ -73,11 +78,15 @@ impl<T, E> ValueWithError<T, E> {
         }
     }
 
+    /// Return true if the instance contains an error
     #[inline]
     pub fn has_err(&self) -> bool {
         self.error.is_some()
     }
 
+    /// Convert into the inner value.
+    ///
+    /// Return `None` if the instance has an error.
     #[inline]
     pub fn ok(self) -> Option<T> {
         match self.error {
@@ -86,11 +95,15 @@ impl<T, E> ValueWithError<T, E> {
         }
     }
 
+    /// Convert into the inner error
+    ///
+    /// Return `None` if the instance does not contains an error
     #[inline]
     pub fn err(self) -> Option<E> {
         self.error
     }
 
+    /// Converts from `&ValueWithError<T, E>` to `ValueWithError<&T, &E>`.
     #[inline]
     pub fn as_ref(&self) -> ValueWithError<&T, &E> {
         ValueWithError {
@@ -99,6 +112,8 @@ impl<T, E> ValueWithError<T, E> {
         }
     }
 
+    /// Converts from `&mut ValueWithError<T, S>` to
+    /// `ValueWithError<&mut T, &mut E>`.
     #[inline]
     pub fn as_mut(&mut self) -> ValueWithError<&mut T, &mut E> {
         ValueWithError {
@@ -107,11 +122,19 @@ impl<T, E> ValueWithError<T, E> {
         }
     }
 
+    /// Extract the inner value regardless of the error.
+    ///
+    /// # Safety
+    ///
+    /// If the instance has an error, the returned value might not satisfy the
+    /// post-condition.
     #[inline]
     pub unsafe fn unwrap_unchecked(self) -> T {
         self.value
     }
 
+    /// Extract the inner value if the instance does not have an error.
+    /// Else, it returns optb.
     #[inline]
     pub fn unwrap_or(self, optb: T) -> T {
         match self.error {
@@ -120,6 +143,8 @@ impl<T, E> ValueWithError<T, E> {
         }
     }
 
+    /// Extract the inner value if the instance does not have an error.
+    /// Else, it calls `op` with the error value.
     #[inline]
     pub fn unwrap_or_else<F: FnOnce(E) -> T>(self, op: F) -> T {
         match self.error {
@@ -128,16 +153,27 @@ impl<T, E> ValueWithError<T, E> {
         }
     }
 
+    /// Extract the content of error.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the instance does not have an error.
     #[inline]
     pub fn unwrap_err(self) -> E {
         self.expect_err("called `ValueWithError::unwrap_err() but it does not contains an error")
     }
 
+    /// Extract the content of error.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the instance does not have an error, with given panic message.
     #[inline]
     pub fn expect_err(self, msg: &str) -> E {
         self.error.expect(msg)
     }
 
+    /// Converts into `std::result::Result<T, E>`.
     #[inline]
     pub fn into_result(self) -> Result<T, E> {
         match self.error {
@@ -148,11 +184,21 @@ impl<T, E> ValueWithError<T, E> {
 }
 
 impl<T, E: Debug> ValueWithError<T, E> {
+    /// Extract the inner value.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the instance has an error.
     #[inline]
     pub fn unwrap(self) -> T {
         self.expect("called `ValueWithError::unwrap()` but it contains an error")
     }
 
+    /// Extract the inner value
+    ///
+    /// # Panics
+    ///
+    /// Panics if the instance has an error, with given panic message.
     pub fn expect(self, msg: &str) -> T {
         if let Some(e) = self.error {
             panic!("{}: {:?}", msg, &e);
@@ -162,10 +208,14 @@ impl<T, E: Debug> ValueWithError<T, E> {
     }
 }
 
+/// Estimation result for integral.
 #[derive(Clone, Debug, PartialEq)]
 pub struct Solution {
+    /// Estimation for the integral
     pub estimate: f64,
+    /// Estimated maximum absolute error for the estimation
     pub delta: f64,
+    /// What times the integrand was evaluated
     pub nevals: usize,
 }
 
@@ -179,4 +229,5 @@ impl Default for Solution {
     }
 }
 
+/// Result of numerical integration
 pub type IntegrationResult = ValueWithError<Solution, RuntimeError>;
