c9b31139c952c50715d5bfd634101bc8ae84d2d5
Kogia-sima <orcinus4627@gmail.com>
2020-03-04 12:24:09 +0900

    Add common types (MaybeIncomplete, Solution)

--- gkquad/src/common.rs
+++ gkquad/src/common.rs
@@ -1,3 +1,6 @@
+use super::error::RuntimeError;
+use std::fmt::Debug;
+
 /// Specify the tolerance which must be satisfied after calculation
 ///
 /// It is recommended to Relative tolerance higher than 1e-14, since roundoff
@@ -44,3 +47,128 @@ impl Default for Tolerance {
         Tolerance::AbsOrRel(1.49e-8, 1.49e-8)
     }
 }
+
+/// `MaybeIncomplete` is a type that holds both partial result (value) and failure
+/// (error).
+///
+/// It is quite similar to `std::result::Result` type, but you can always
+/// extract the partial result by calling `unwrap_unchecked()` method.
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord, Hash)]
+pub struct MaybeIncomplete<T, E> {
+    pub(crate) value: T,
+    pub(crate) error: Option<E>,
+}
+
+impl<T, E> MaybeIncomplete<T, E> {
+    #[inline]
+    pub fn new(value: T) -> Self {
+        Self { value, error: None }
+    }
+
+    #[inline]
+    pub fn with_error(value: T, error: E) -> Self {
+        Self {
+            value,
+            error: Some(error),
+        }
+    }
+
+    #[inline]
+    pub fn has_err(&self) -> bool {
+        self.error.is_some()
+    }
+
+    #[inline]
+    pub fn ok(self) -> Option<T> {
+        match self.error {
+            Some(_) => None,
+            None => Some(self.value),
+        }
+    }
+
+    #[inline]
+    pub fn err(self) -> Option<E> {
+        self.error
+    }
+
+    #[inline]
+    pub fn as_ref(&self) -> MaybeIncomplete<&T, &E> {
+        MaybeIncomplete {
+            value: &self.value,
+            error: self.error.as_ref(),
+        }
+    }
+
+    #[inline]
+    pub fn as_mut(&mut self) -> MaybeIncomplete<&mut T, &mut E> {
+        MaybeIncomplete {
+            value: &mut self.value,
+            error: self.error.as_mut(),
+        }
+    }
+
+    #[inline]
+    pub unsafe fn unwrap_unchecked(self) -> T {
+        self.value
+    }
+
+    #[inline]
+    pub fn unwrap_or(self, optb: T) -> T {
+        match self.error {
+            Some(_) => optb,
+            None => self.value,
+        }
+    }
+
+    #[inline]
+    pub fn unwrap_or_else<F: FnOnce(E) -> T>(self, op: F) -> T {
+        match self.error {
+            Some(e) => op(e),
+            None => self.value,
+        }
+    }
+
+    #[inline]
+    pub fn unwrap_err(self) -> E {
+        self.expect_err("called `MaybeIncomplete::unwrap_err() but it does not contains an error")
+    }
+
+    #[inline]
+    pub fn expect_err(self, msg: &str) -> E {
+        self.error.expect(msg)
+    }
+}
+
+impl<T, E: Debug> MaybeIncomplete<T, E> {
+    #[inline]
+    pub fn unwrap(self) -> T {
+        self.expect("called `MaybeIncomplete::unwrap()` but it contains an error")
+    }
+
+    pub fn expect(self, msg: &str) -> T {
+        if let Some(e) = self.error {
+            panic!("{}: {:?}", msg, &e);
+        }
+
+        self.value
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Solution {
+    pub estimate: f64,
+    pub delta: f64,
+    pub nevals: usize,
+}
+
+impl Default for Solution {
+    fn default() -> Self {
+        Self {
+            estimate: 0.0,
+            delta: core::f64::MAX,
+            nevals: 0,
+        }
+    }
+}
+
+pub type IntegrationResult = MaybeIncomplete<Solution, RuntimeError>;
